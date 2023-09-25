3d791314abe890c0b56bea1f62604a747553bf8f
Xau <frozzy.xfs@gmail.com>
2023-09-24 22:24:10 -0400

    Add coordinate implementation

--- src/coordinate.js
+++ src/coordinate.js
@@ -2,6 +2,23 @@ class Coordinate {
     constructor(x, y) {
         this.x = x;
         this.y = y;
+        this.data = [null, false];
+    }
+
+    getData() {
+        return this.data;
+    }
+
+    setShip(ship) {
+        this.data[0] = ship;
+    }
+
+    shoot() {
+        this.data[1] = true;
+    }
+
+    isShot() {
+        return this.data[1];
     }
 }
 
