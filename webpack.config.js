7f33c45b32b57194209441edd157e7f99eabfc69
Xau <frozzy.xfs@gmail.com>
2023-09-24 20:38:27 -0400

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js'
+    },
+    output: {
+        filename: 'index.js',
+            path: path.resolve(__dirname, 'dist'),
+            clean: true,
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Battleship',
+            template: './src/index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+			{
+				test: /\.(?:js|mjs|cjs)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: [
+							["@babel/preset-env", { targets: "defaults" }], // or defaults
+						]
+					},
+				},
+			},
+        ],
+    },
+};
