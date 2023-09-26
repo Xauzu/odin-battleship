13ec443bbff4f5235a8e3bad45866fcb1a8710e8
Xau <frozzy.xfs@gmail.com>
2023-09-26 06:12:35 -0400

    Add player implementation

---
+++ src/player.js
@@ -0,0 +1,15 @@
+class Player {
+    constructor(name) {
+        this.name = name;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+
+    getName() {
+        return this.name;
+    }
+}
+
+export {Player};
