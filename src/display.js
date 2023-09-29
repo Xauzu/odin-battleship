2ba54e070fedb373d1e136afdfeecb3d3e2db917
Xau <23320206+Xauzu@users.noreply.github.com>
2023-09-29 00:16:50 -0400

    Update upateDisplay method, add helper function

--- src/display.js
+++ src/display.js
@@ -17,43 +17,37 @@ const mainSetup = () => {
 
 const setupDisplay = (displayID, length, width) => {
     const boardDisplay = document.querySelector(`#board-display-${displayID}`);
+    boardDisplay.style.display = 'grid';
     boardDisplay.style.gridTemplateColumns = `repeat(${length}, auto)`;
     boardDisplay.style.gridTemplateRows = `repeat(${width}, auto)`;
 };
+
+const createGameboardObject = (element) => {
+    const gameboardItem = document.createElement('div');
+    gameboardItem.classList.add('gameboard-item');
+    gameboardItem.setAttribute('data-x', element.x);
+    gameboardItem.setAttribute('data-y', element.y);
+    gameboardItem.setAttribute('data-hit', element.data[1]);
+
+    return gameboardItem;
+}
 
 // display id = 0 || 1
 const updateDisplay = (displayID, board) => {
     const boardDisplay = document.querySelector(`#board-display-${displayID}`);
     boardDisplay.innerHTML = '';
 
-    const playerID = board.getBoardPlayerID();
-    if (playerID === displayID)
-        board.getGrid().forEach(element => {
-            const gridElement = document.createElement('div');
-            gridElement.setAttribute('data-x', element.x);
-            gridElement.setAttribute('data-y', element.y);
-            gridElement.setAttribute('data-hit', element.data[1]);
+    board.getGrid().forEach(element => {
+        const gameboardItem = createGameboardObject(element);
 
-            // Temp?
-            if (element.data[0])
-                gridElement.textContent = 'S';
+        // Temp?
+        if (element.data[displayID] && displayID === 0)
+            gameboardItem.textContent = 'S';
+        else if (element.data[displayID] && displayID === 1)
+            gameboardItem.textContent = 'X';
 
-            boardDisplay.appendChild(gridElement);
-        });
-    else {
-        board.getGrid().forEach(element => {
-            const gridElement = document.createElement('div');
-            gridElement.setAttribute('data-x', element.x);
-            gridElement.setAttribute('data-y', element.y);
-            gridElement.setAttribute('data-hit', element.data[1]);
-
-            // Temp?
-            if (element.data[1])
-                gridElement.textContent = 'X';
-
-            boardDisplay.appendChild(gridElement);
-        });
-    }
+        boardDisplay.appendChild(gameboardItem);
+    });
 };
 
 const updateMessage = (message) => {
