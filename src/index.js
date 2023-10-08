b803a3bd672cea5594a597a00c320539556bf74c
Xau <23320206+Xauzu@users.noreply.github.com>
2023-10-07 20:29:15 -0400

    Add generateShipPlacements helper

--- src/index.js
+++ src/index.js
@@ -3,7 +3,24 @@ import { Gameboard } from './gameboard';
 
 import './style.css';
 
-const test = 1;
+const test = 0;
+
+const generateShipPlacements = (gameboard, shipLengthArray) => {
+    const shipLengths = [...shipLengthArray];
+    while (shipLengths.length > 0) {
+        const shipLength = shipLengths[0];
+        const x = Math.floor(Math.random() * (gameboard.getLength() - shipLength));
+        const y = Math.floor(Math.random() * (gameboard.getWidth() - shipLength));
+        const vert = !!Math.floor(Math.random() * 2);
+
+        console.log(shipLengths, 'Attempt:', shipLength, x, y, vert);
+
+        const ship = gameboard.placeShip(x, y, shipLength, vert);
+        if (ship !== false) {
+            shipLengths.shift();
+        }
+    }
+}
 
 const setup = () => {
     const length = 10;
@@ -13,21 +30,22 @@ const setup = () => {
 
     updateMessage('Setting up ...');
 
+    const gameboards = [];
     for (let i = 0; i < 2; i++) {
         const gameboard = new Gameboard(length, width, i);
+        gameboards.push(gameboard);
+
         setupDisplay(i, length, width);
-
-        if (test) {
-            // Preplaced test
-            gameboard.placeShip(0 + i, 0 + i, 5, true);
-            gameboard.placeShip(1 + i, 1 + i, 4, false);
-            gameboard.placeShip(2 + i, 2 + i, 3, true);
-            gameboard.placeShip(3 + i, 3 + i, 3, false);
-            gameboard.placeShip(4 + i, 4 + i, 2, true);
-        }
-
-        updateDisplay(i, gameboard);
     }
+
+    // Ai board
+    const shipLengths = [5, 4, 3, 3, 2];
+    for (let i = test; i < 2; i++) {
+        generateShipPlacements(gameboards[i], shipLengths);
+        updateDisplay(i, gameboards[i]);
+    }
+
+    updateMessage('Finished setting up');
 };
 
 setup();
